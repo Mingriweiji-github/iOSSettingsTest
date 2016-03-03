7f29e433f47881fa5a491ca7c032f0f175f3adb2
Mingriweiji-github <18046506190@163.com>
2016-03-03 15:23:33 +0800

    直接打开并设置VPN
    直接打开并设置VPN

--- Test/ViewController.m
+++ Test/ViewController.m
@@ -46,7 +46,7 @@
 //    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"prefs:root=INTERNET_TETHERING"]];
     
     //跳转WIFI
-    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"prefs:root=WIFI"]];
+//    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"prefs:root=WIFI"]];
     
 
     //蓝牙
@@ -54,6 +54,81 @@
     
   //Safari浏览器
 //    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"prefs:root=Safari"]];
+    
+    //直接打开VPN
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"prefs:root=VPN"]];
+    
+//    跳转到其他的界面的字段修改prefs:root=
+//    
+//    About — prefs:root=General&path=About
+//    
+//    Accessibility — prefs:root=General&path=ACCESSIBILITY
+//    
+//    AirplaneModeOn— prefs:root=AIRPLANE_MODE
+//    
+//    Auto-Lock — prefs:root=General&path=AUTOLOCK
+//    
+//    Brightness — prefs:root=Brightness
+//    
+//    Bluetooth — prefs:root=General&path=Bluetooth
+//    
+//    Date& Time — prefs:root=General&path=DATE_AND_TIME
+//    
+//    FaceTime — prefs:root=FACETIME
+//    
+//    General— prefs:root=General
+//    
+//    Keyboard — prefs:root=General&path=Keyboard
+//    
+//    iCloud — prefs:root=CASTLE  iCloud
+//    
+//    Storage & Backup — prefs:root=CASTLE&path=STORAGE_AND_BACKUP
+//    
+//    International — prefs:root=General&path=INTERNATIONAL
+//    
+//    Location Services — prefs:root=LOCATION_SERVICES
+//    
+//    Music — prefs:root=MUSIC
+//    
+//    Music Equalizer — prefs:root=MUSIC&path=EQ
+//    
+//    Music VolumeLimit— prefs:root=MUSIC&path=VolumeLimit
+//    
+//    Network — prefs:root=General&path=Network
+//    
+//    Nike + iPod — prefs:root=NIKE_PLUS_IPOD
+//    
+//    Notes — prefs:root=NOTES
+//    
+//    Notification — prefs:root=NOTIFICATIONS_ID
+//    
+//    Phone — prefs:root=Phone
+//    
+//    Photos — prefs:root=Photos
+//    
+//    Profile — prefs:root=General&path=ManagedConfigurationList
+//    
+//    Reset — prefs:root=General&path=Reset
+//    
+//    Safari — prefs:root=Safari  Siri — prefs:root=General&path=Assistant
+//    
+//    Sounds — prefs:root=Sounds
+//    
+//    SoftwareUpdate— prefs:root=General&path=SOFTWARE_UPDATE_LINK
+//    
+//    Store — prefs:root=STORE
+//    
+//    Twitter — prefs:root=TWITTER
+//    
+//    Usage — prefs:root=General&path=USAGE
+//    
+//    VPN — prefs:root=General&path=Network/VPN
+//    
+//    Wallpaper — prefs:root=Wallpaper
+//    
+//    Wi-Fi — prefs:root=WIFI
+//    
+//    Setting—prefs:root=INTERNET_TETHERING
     
     
 }
